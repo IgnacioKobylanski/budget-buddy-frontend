9b52c8465113dbc40a70cb4c2dc6d26d2ef6c6ac
IgnacioKobylanski <124396067+IgnacioKobylanski@users.noreply.github.com>
2025-11-06 17:11:24 -0300

    minor changes in main.tsx

--- budgetbuddy-frontend/src/components/Main/Main.tsx
+++ budgetbuddy-frontend/src/components/Main/Main.tsx
@@ -4,7 +4,7 @@ import "./Main.css";
 const Main: React.FC = () => {
   return (
     <main className="main-content">
-      <h1>Bienvenido a Budget Buddy</h1>
+      <h1>Bienvenido a Budget Buddy!</h1>
       <p>
         Aquí podrás gestionar tus transacciones, categorías y usuarios de forma sencilla.
       </p>
