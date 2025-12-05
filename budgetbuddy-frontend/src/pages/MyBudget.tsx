06a1f17ee89c94f5c1266bf1bf568dedfcfa8046
IgnacioKobylanski <124396067+IgnacioKobylanski@users.noreply.github.com>
2025-12-05 18:26:28 -0300

    minor change in  MyBudget component

--- budgetbuddy-frontend/src/pages/MyBudget.tsx
+++ budgetbuddy-frontend/src/pages/MyBudget.tsx
@@ -4,7 +4,6 @@ const MyBudget = () =>{
     return(
         <div className="my-budget-main">
             <h1>My Budget</h1>
-            <p>Take a look at your budget buddy</p>
         </div>
         
     );
